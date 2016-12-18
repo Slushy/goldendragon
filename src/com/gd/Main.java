85fd1974eedaa4bde9b19fccd80843323fa00c3b
Brandon Porter <brandon.porter@hulu.com>
2016-12-18 13:40:43 -0800

    Created entry point for engine

---
+++ src/com/gd/Main.java
@@ -0,0 +1,12 @@
+package com.gd;
+
+/*
+ * Entry point for running the GD game engine by itself
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
